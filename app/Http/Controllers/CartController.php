180202444d04e6760eb3632ae16a86a545ae90ff
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-23 10:25:56 -0300

    Aula 5 - Método all (recupera todos os dados) e método has (verifica se o item está setado e não é nulo)

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,9 +9,12 @@ class CartController extends Controller
     public function list(Request $request)
     {
         //$data = $request->session()->get('product', 'Produto não encontrado.');
-        $product = session('product', 'Produto não encontrado');
-        $total = session('total', 'Nenhum valor no carrinho');
-        dd($product, $total);
+        //$product = session('product', 'Produto não encontrado');
+        //$total = session('total', 'Nenhum valor no carrinho');
+
+        $data = session()->all();
+
+        dd($data);
     }
 
     public function add(Request $request)
@@ -26,10 +29,15 @@ class CartController extends Controller
 
     public function remove(Request $request)
     {
+
+
+        if (!session()->has('product')) {
+           return 'Nenhum produto para remover';
+        }
+
         //$request->session()->forget(['product', 'total']);
         session()->forget(['product', 'total']);
         //session()->flush();
-
         return "Removido com sucesso";
     }
 }
