7cf1d40027f9cd6459abf7303f4257caa3264fae
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-23 09:57:36 -0300

    Aula 1 - Estrutura inicial

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\CartController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/cart/list', [CartController::class, 'list'])->name('cart.list');
+Route::get('/cart/add', [CartController::class, 'add'])->name('cart.add');
