40e4b7642512b283087df8715ab9d1e7f57923ba
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-23 10:04:15 -0300

    Aula 3 - Leitura e escrita de dados na sessão através do helper session

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,14 +8,17 @@ class CartController extends Controller
 {
     public function list(Request $request)
     {
-        $data = $request->session()->get('product', 'Produto não encontrado.');
-
+        //$data = $request->session()->get('product', 'Produto não encontrado.');
+        $data = session('product', 'Produto não encontrado');
         dd($data);
     }
 
     public function add(Request $request)
     {
-        $request->session()->put('product', 'Action Figure');
+        //$request->session()->put('product', 'Action Figure');
+        session([
+            'product' => "Bola"
+        ]);
         return "Adicionado com sucesso!";
     }
 }
